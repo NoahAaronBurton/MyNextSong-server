8722788e09a822d36284a96fb709fa31f0d7e0a4
Noah Burton <nburton115@gmail.com>
2024-01-24 16:14:29 -0700

    server.js: express, test route

---
+++ server.js
@@ -0,0 +1,11 @@
+//todo: create express server
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+
+app.listen(3000, () => console.log('Server started on port 3000'));
